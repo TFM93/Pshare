fd8f756121e747fa38bee0bb5c52ae1507b0fa53
Tiago Magalhaes <tiagoferreiramagalhaes@ua.pt>
2016-05-27 17:49:16 +0100

    third view added and respective controlled associated

---
+++ Pshare/Pshare/ConnectionsViewController.h
@@ -0,0 +1,13 @@
+//
+//  ConnectionsViewController.h
+//  Pshare
+//
+//  Created by Tiago Magalhães on 27/05/16.
+//  Copyright © 2016 Tiago Magalhães. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ConnectionsViewController : UIViewController
+
+@end
---
+++ Pshare/Pshare/ConnectionsViewController.m
@@ -0,0 +1,37 @@
+//
+//  ConnectionsViewController.m
+//  Pshare
+//
+//  Created by Tiago Magalhães on 27/05/16.
+//  Copyright © 2016 Tiago Magalhães. All rights reserved.
+//
+
+#import "ConnectionsViewController.h"
+
+@interface ConnectionsViewController ()
+
+@end
+
+@implementation ConnectionsViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
