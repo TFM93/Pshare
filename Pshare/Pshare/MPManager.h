3114a48d44e7c6abb2c60b4f38c036d5c6d53f2e
Tiago Magalhaes <tiagoferreiramagalhaes@ua.pt>
2016-05-27 20:31:25 +0100

    multipeer manager implemented, not working yet

---
+++ Pshare/Pshare/MPManager.h
@@ -0,0 +1,13 @@
+//
+//  MPManager.h
+//  Pshare
+//
+//  Created by Tiago Magalhães on 27/05/16.
+//  Copyright © 2016 Tiago Magalhães. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MPManager : NSObject
+
+@end
---
+++ Pshare/Pshare/MPManager.m
@@ -0,0 +1,13 @@
+//
+//  MPManager.m
+//  Pshare
+//
+//  Created by Tiago Magalhães on 27/05/16.
+//  Copyright © 2016 Tiago Magalhães. All rights reserved.
+//
+
+#import "MPManager.h"
+
+@implementation MPManager
+
+@end
